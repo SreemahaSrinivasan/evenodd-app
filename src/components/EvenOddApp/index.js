8868708cb7cefe148f649e6e9b35172b79e1c968
SreemahaSrinivasan <srimahas2001@gmail.com>
2025-03-24 15:10:38 +0000

    code updated

--- src/components/EvenOddApp/index.js
+++ src/components/EvenOddApp/index.js
@@ -1,1 +1,37 @@
 // Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class EvenOddApp extends Component {
+  state = {count: 0}
+
+  onClickIncrementButton = () => {
+    const randomNumber = Math.ceil(Math.random() * 100)
+    this.setState(prevState => ({
+      count: prevState.count + randomNumber,
+    }))
+  }
+
+  render() {
+    const {count} = this.state
+    const isEvenOrOdd = count % 2 === 0 ? 'Even' : 'Odd'
+    return (
+      <div className="app-container">
+        <div className="count-container">
+          <h1 className="count">Count {count}</h1>
+          <p className="number-category">Count is {isEvenOrOdd}</p>
+          <button
+            type="button"
+            className="increment-button"
+            onClick={this.onClickIncrementButton}
+          >
+            Increment
+          </button>
+          <p className="note">*Increase By Random Number Between 0 to 100</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
